2c10447b68ed01958f172cc0364f4b1b8328b4ef
cesardddp <cesardddp@hotmail.com>
2021-08-24 20:38:20 -0300

    allow cursos/ and not allow post topicos

--- src/main/java/br/com/unesp/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/unesp/forum/config/security/SecurityConfigurations.java
@@ -50,8 +50,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/cursos").permitAll()
 		.antMatchers(HttpMethod.GET,"/topicos").permitAll()
-		.antMatchers(HttpMethod.POST,"/topicos").permitAll()
+		// .antMatchers(HttpMethod.POST,"/topicos").permitAll()
 		.antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST,"/auth").permitAll()
 		.antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
